878786f694515d217769aad5ed520940ae6f229a
David3 <polidavide567@gmail.com>
2024-10-16 19:40:21 +0200

    progetto finale

---
+++ src/app/client.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+
+export interface Client {
+  id: number;
+  nome: string;
+  cognome: string;
+  email: string;
+  azienda: string;
+  userId: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+
+  private apiUrl = 'http://localhost:3000/clients';
+
+  constructor(private http: HttpClient) { }
+
+  getClientsById(id: number): Observable<Client[]> {
+    const url = `${this.apiUrl}?userId=${id}`;
+    return this.http.get<Client[]>(url);
+  }
+  
+  addClient(client: Client): Observable<Client> {
+    return this.http.post<Client>(this.apiUrl, client);
+  }
+
+  updateClient(client: Client): Observable<Client> {
+    const url = `${this.apiUrl}/${client.id}`;
+    return this.http.put<Client>(url, client);
+  }
+  deleteClient(id: number): Observable<void> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete<void>(url);
+  }
+}
---
+++ src/app/clienti/clienti.component.ts
@@ -0,0 +1,114 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Client, ClientService } from '../client.service';
+import { Observable } from 'rxjs';
+import './clienti.component.css';
+
+@Component({
+  selector: 'app-clienti',
+  templateUrl: './clienti.component.html',
+  styleUrl: './clienti.component.css',
+})
+export class ClientiComponent implements OnInit {
+
+  displayedColumns: string[] = ['id', 'nome', 'cognome', 'email', 'azienda', 'edit', 'delete'];
+
+  clienti: Client[] = [];
+  clienteSelezionato: any;
+  editForm: any;
+
+  isvisible: boolean = false;
+  selectedClientId: number | null = null;
+
+  userForm: FormGroup;
+  isEditMode: boolean = false;
+
+  constructor(private clientService: ClientService, private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      nome: ['', Validators.required],
+      cognome: ['', Validators.required],
+      email: ['', Validators.required],
+      azienda: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+    this.getClient();
+  }
+
+  show(): void {
+    this.isvisible = !this.isvisible;
+  }
+
+  getClient(): void {
+    let id = sessionStorage.getItem('userId');
+    this.clientService.getClientsById(parseInt(id as string)).subscribe({
+      next: (clients) => {
+        this.clienti = clients;
+      },
+      error: (error: any) => {
+        console.error('Errore recupero clienti', error);
+      }
+    });
+  }
+
+  onEdit(clienti: Client): void {
+    this.isvisible = true;
+    this.userForm.patchValue({
+      nome: clienti.nome,
+      cognome: clienti.cognome,
+      email: clienti.email,
+      azienda: clienti.azienda
+    });
+    this.selectedClientId = clienti.id !== undefined ? clienti.id : null;
+    this.isEditMode= true;
+  }
+
+  onDelete(id: number): void {
+    this.clientService.deleteClient(id).subscribe({
+      next: () => {
+        this.clienti = this.clienti.filter(c => c.id !== id);
+        console.log(`Cliente con id ${id} cancellato con successo`);
+      },
+      error: (error: any) => {
+        console.error('Errore durante la cancellazione del cliente', error);
+      }
+    });
+  }
+  
+
+  onSubmit(): void {
+    if (this.selectedClientId === null) {
+      let id = sessionStorage.getItem('userId');
+      const cliente: Client = { ...this.userForm.value, userId: id };
+      this.clientService.addClient(cliente).subscribe({
+        next: () => {
+          this.getClient();
+          this.resetForm();
+        },
+        error: (error: any) => {
+          console.error('Errore aggiunta cliente', error);
+        }
+      });
+    } else {
+      let userId = sessionStorage.getItem('userId');
+      const cliente = { ...this.userForm.value, userId: userId, id: this.selectedClientId };
+      this.clientService.updateClient(cliente).subscribe({
+        next: () => {
+          this.getClient();
+          this.selectedClientId = null;
+          this.resetForm();
+        },
+        error: (error: any) => {
+          console.error("Errore durante l'aggiornamento del cliente:", error);
+        }
+      });
+    }
+  }
+  
+  // Metodo per resettare il form e nasconderlo
+  resetForm(): void {
+    this.userForm.reset();    
+    this.isvisible = false;   
+  }
+}
